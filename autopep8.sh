5c570dc90c00b62c1834f959d1c0e28ff25c9f5b
Sadra Sabouri <sabouri.sadra@gmail.com>
2023-05-30 01:22:02 +0330

    add : pep8 bat/shell.

---
+++ autopep8.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python -m autopep8 nava --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
+python -m autopep8 others --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
