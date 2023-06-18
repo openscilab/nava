e280126afea2e1d3c1b410b0f32a60ebe749138e
AHReccese <amirhssin6rst@gmail.com>
2023-06-19 00:31:33 +0330

    Decorator issue with keyword arguments fixed

--- nava/params.py
+++ nava/params.py
@@ -11,3 +11,4 @@ A Python library for playing sound everywhere natively and securely.
 SOUND_FILE_PLAY_ERROR = "Sound can not play due to some issues."
 SOUND_FILE_EXIST_ERROR = "Given sound file doesn't exist."
 SOUND_FILE_PATH_TYPE_ERROR = "Sound file's path should be a string."
+SOUND_FILE_PATH_NOT_GIVEN = "There isn't any sound file path given to the play function."
