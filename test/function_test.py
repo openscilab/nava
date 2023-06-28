a9c44263c51c0fe198ecdee722c4765d0bfd6aaf
sepandhaghighi <sepand.haghighi@yahoo.com>
2023-06-28 04:46:11 +0430

    fix : nava functions test updated

--- test/function_test.py
+++ test/function_test.py
@@ -3,4 +3,12 @@
 >>> import os
 >>> from nava import play
 >>> play(os.path.join("others", "test.wav"))
+>>> from nava.functions import nava_help
+>>> nava_help()
+<BLANKLINE>
+A Python library for playing sound everywhere natively and securely.
+<BLANKLINE>
+<BLANKLINE>
+Repo : https://github.com/openscilab/nava
+Webpage : https://openscilab.com/
 """
