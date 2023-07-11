bcb5fa84e81fefc4ed68b1650e50aafac2b19ba7
Sadra Sabouri <sabouri.sadra@gmail.com>
2023-07-11 17:39:31 +0330

    add : `play_async` added to `__init__.py`.

--- nava/__init__.py
+++ nava/__init__.py
@@ -2,6 +2,6 @@
 """Nava modules."""
 from .params import NAVA_VERSION
 from .errors import NavaBaseError
-from .functions import play
+from .functions import play, play_async
 
 __version__ = NAVA_VERSION
