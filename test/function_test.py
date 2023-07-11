0a52f58ffe908cdfcbcb6e77c64b86e5b3216165
Sadra Sabouri <sabouri.sadra@gmail.com>
2023-07-11 18:05:39 +0330

    update : tests updated.

--- test/function_test.py
+++ test/function_test.py
@@ -6,19 +6,13 @@
 >>> from nava import play, play_async
 >>> PATH2TEST_WAV = os.path.join("others", "test.wav")
 >>> play(PATH2TEST_WAV)
->>> async def test_print():
-...    start_time = time()
+>>> async def sleep_test():
 ...    while True:
-...        print("Test voice is in {:.1f}s".format(time() - start_time))
 ...        await asyncio.sleep(0.1)
 >>> async def play_async_test():
-...    asyncio.create_task(test_print())
+...    asyncio.create_task(sleep_test())
 ...    await asyncio.create_task(play_async(PATH2TEST_WAV))
 >>> asyncio.run(play_async_test())
-Test voice is in 0.0s
-Test voice is in 0.1s
-Test voice is in 0.2s
-Test voice is in 0.3s
 >>> from nava.functions import nava_help
 >>> nava_help()
 <BLANKLINE>
