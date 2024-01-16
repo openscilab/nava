b9f52df8a9e0b7616d0d9ea2297d6343facf5e59
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-01-17 00:16:10 +0330

    doc : NavaThread docstring bug fixed

--- nava/thread.py
+++ nava/thread.py
@@ -6,9 +6,7 @@ import threading
 
 
 class NavaThread(threading.Thread):
-    """
-    Nava custom thread.
-    """
+    """Nava custom thread."""
 
     def __init__(self, *args, **kwargs):
         """
