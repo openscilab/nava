8175182a408b492d5c5f73af62abfe399a26eb85
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-01-17 00:25:50 +0330

    fix : function_test updated

--- test/function_test.py
+++ test/function_test.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 >>> import os
->>> from nava import play
->>> sound_id = play(os.path.join("others", "test.wav"))
+>>> from nava import play, stop, stop_all
+>>> play(os.path.join("others", "test.wav"), async_mode=False)
+>>> sound_id_1 = play(os.path.join("others", "test.wav"), async_mode=True)
+>>> sound_id_1 == 1001
+True
+>>> sound_id_2 = play(os.path.join("others", "test.wav"), async_mode=True)
+>>> sound_id_2 == 1002
+True
+>>> sound_id_3 = play(os.path.join("others", "test.wav"), async_mode=True)
+>>> sound_id_3 == 1003
+True
+>>> stop(1001)
+>>> stop_all()
 >>> from nava.functions import nava_help
 >>> nava_help()
 <BLANKLINE>
