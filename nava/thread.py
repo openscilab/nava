8c5fa3ac4e9e09a04f51b0944add436b536c353b
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-01-16 23:56:35 +0330

    doc : NavaThread class docstring updated

--- nava/thread.py
+++ nava/thread.py
@@ -6,15 +6,36 @@ import threading
 
 
 class NavaThread(threading.Thread):
+    """
+    Nava custom thread.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Init method.
+
+        :param args: arguments
+        :type args: list
+        :param kwargs: keyword arguments
+        :type kwargs: dict
+        """
         super(NavaThread, self).__init__(*args, **kwargs)
         self.play_process = None
 
     def run(self):
+        """
+        Run target function.
+
+        :return: None
+        """
         if self._target is not None:
             self.play_process = self._target(*self._args, **self._kwargs)
 
     def stop(self):
+        """
+        Stop sound.
+
+        :return: None
+        """
         sys_platform = sys.platform
         if sys_platform == "win32":
             import winsound
