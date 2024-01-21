a458f59c3c67c1f4520f6aaf626ca2e98eb7c301
Sadra Sabouri <sabouri.sadra@gmail.com>
2024-01-20 17:33:42 -0800

    test : tests added.

--- test/function_test.py
+++ test/function_test.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 >>> import os
+>>> import time
 >>> from nava import play, stop, stop_all
 >>> play(os.path.join("others", "test.wav"))
 >>> sound_id_1 = play(os.path.join("others", "test.wav"), async_mode=True)
 >>> sound_id_1 == 1001
 True
->>> sound_id_2 = play(os.path.join("others", "test.wav"), async_mode=True)
+>>> sound_id_2 = play(os.path.join("others", "test.wav"), async_mode=True, loop=True)
 >>> sound_id_2 == 1002
 True
->>> sound_id_3 = play(os.path.join("others", "test.wav"), async_mode=True)
->>> sound_id_3 == 1003
-True
+>>> time.sleep(1)
 >>> stop(1001)
 >>> stop_all()
 >>> from nava.functions import nava_help
