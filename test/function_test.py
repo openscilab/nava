cf9e195fff0668d68a27002409215a67bc8d124e
Sepand Haghighi <sepand.haghighi@yahoo.com>
2024-02-08 10:20:05 +0330

    Orphan processes (#35)
    * fix : tests modified
    
    * fix : tests modified
    
    * fix : minor bug in tests fixed
    
    * fix : tests modified
    
    * fix : tests modified
    
    * fix : tests modified
    
    * fix : tests modified
    
    * fix : NavaThread stop method bug fixed
    
    * doc : CHANGELOG.md updated
    
    * fix : tests updated

--- test/function_test.py
+++ test/function_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 >>> import os
+>>> import time
 >>> from nava import play, stop, stop_all
 >>> play(os.path.join("others", "test.wav"))
 >>> sound_id_1 = play(os.path.join("others", "test.wav"), async_mode=True)
@@ -14,6 +15,10 @@ True
 True
 >>> stop(1001)
 >>> stop_all()
+>>> for i in range(40):
+...     sound_id = play(os.path.join("others", "test.wav"), async_mode=True)
+...     time.sleep(0.5)
+...     stop(sound_id)
 >>> from nava.functions import nava_help
 >>> nava_help()
 <BLANKLINE>
