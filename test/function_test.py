582451ab77132d8db469ff2f174fd986b53a8636
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-02-10 19:15:46 +0330

    fix : minor bug in function_test.py fixed

--- test/function_test.py
+++ test/function_test.py
@@ -15,15 +15,14 @@ True
 True
 >>> nava.stop(sound_id_1)
 >>> for i in range(40):
-...     sound_id = play(os.path.join("others", "test.wav"), async_mode=True)
+...     sound_id = nava.play(os.path.join("others", "test.wav"), async_mode=True)
 ...     time.sleep(0.2)
 >>> nava.stop_all()
 >>> len(nava._play_threads_map) == 43
 True
 >>> nava._play_threads_counter == 43
 True
->>> from nava.functions import nava_help
->>> nava_help()
+>>> nava.functions.nava_help()
 <BLANKLINE>
 A Python library for playing sound everywhere natively and securely.
 <BLANKLINE>
