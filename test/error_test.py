b7a8680968e7add4725bdd0658791d061de90697
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-03-26 19:48:39 +0430

    fix : error_test.py updated

--- test/error_test.py
+++ test/error_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+>>> import os
 >>> from nava import play, stop
 >>> play("test.wav")
 Traceback (most recent call last):
@@ -13,4 +14,8 @@ nava.errors.NavaBaseError: Sound file's path should be a string.
 Traceback (most recent call last):
     ...
 nava.errors.NavaBaseError: Given sound id doesn't exist.
+>>> nava.play(os.path.join("others", "test.wav"), async_mode=False, loop=True)
+Traceback (most recent call last):
+    ...
+nava.errors.NavaBaseError: The loop option can only be used in async mode.
 """
