2071c815cef3691da875de31d601e282853f5e9a
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-03-29 18:28:17 +0430

    fix : play_process renamed to _play_process

--- nava/thread.py
+++ nava/thread.py
@@ -21,7 +21,7 @@ class NavaThread(threading.Thread):
         """
         super(NavaThread, self).__init__(*args, **kwargs)
         self._sys_platform = sys.platform
-        self.play_process = None
+        self._play_process = None
         self._loop = loop
 
     def run(self):
@@ -32,11 +32,11 @@ class NavaThread(threading.Thread):
         """
         if self._target is not None:
             if self._sys_platform == "win32":
-                self.play_process = self._target(*self._args, **self._kwargs)
+                self._play_process = self._target(*self._args, **self._kwargs)
             else:
                 while True:
-                    self.play_process = self._target(*self._args, **self._kwargs)
-                    self.play_process.wait()
+                    self._play_process = self._target(*self._args, **self._kwargs)
+                    self._play_process.wait()
                     if not self._loop:
                         break
 
@@ -51,14 +51,14 @@ class NavaThread(threading.Thread):
             import winsound
             winsound.PlaySound(None, winsound.SND_PURGE)
         else:
-            if self.play_process is not None:
+            if self._play_process is not None:
                 try:
-                    self.play_process.stdout.close()
-                    self.play_process.stdin.close()
-                    self.play_process.stderr.close()
-                    self.play_process.kill()
-                    self.play_process.terminate()
+                    self._play_process.stdout.close()
+                    self._play_process.stdin.close()
+                    self._play_process.stderr.close()
+                    self._play_process.kill()
+                    self._play_process.terminate()
                 except ProcessLookupError:
                     pass
                 finally:
-                    self.play_process.wait()
+                    self._play_process.wait()
