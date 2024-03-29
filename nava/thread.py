2e18026bd541272ff59d2e99b080877dcd9b9fdf
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-03-29 18:26:27 +0430

    fix : sys_platform renamed to _sys_platform

--- nava/thread.py
+++ nava/thread.py
@@ -20,7 +20,7 @@ class NavaThread(threading.Thread):
         :type kwargs: dict
         """
         super(NavaThread, self).__init__(*args, **kwargs)
-        self.sys_platform = sys.platform
+        self._sys_platform = sys.platform
         self.play_process = None
         self.loop = loop
 
@@ -31,7 +31,7 @@ class NavaThread(threading.Thread):
         :return: None
         """
         if self._target is not None:
-            if self.sys_platform == "win32":
+            if self._sys_platform == "win32":
                 self.play_process = self._target(*self._args, **self._kwargs)
             else:
                 while True:
@@ -47,7 +47,7 @@ class NavaThread(threading.Thread):
         :return: None
         """
         self.loop = False
-        if self.sys_platform == "win32":
+        if self._sys_platform == "win32":
             import winsound
             winsound.PlaySound(None, winsound.SND_PURGE)
         else:
