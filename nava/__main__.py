496252a10b83a2e4dc518d0eea92493a1c787e64
AHReccese <amirhssin6rst@gmail.com>
2024-04-28 05:04:02 +0330

    add --file argument to arg parser

--- nava/__main__.py
+++ nava/__main__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """Nava main."""
-
-import sys
+import argparse
 from art import tprint
 from .params import NAVA_VERSION
-from .functions import nava_help
+from .functions import nava_help, run_nava
 
 
 def main():
@@ -13,10 +12,20 @@ def main():
 
     :return: None
     """
-    args = sys.argv
     tprint("nava")
     tprint("V:" + NAVA_VERSION)
     nava_help()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--file',
+        nargs = 1,
+        metavar = "audio file name",
+        type=str,
+        help="name of the audio file",
+        )
+    # parse the arguments from the standard input
+    args = parser.parse_args()
+    run_nava(args)
 
 
 if __name__ == "__main__":
