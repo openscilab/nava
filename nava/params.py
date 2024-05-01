5f15544a136d142a35fb32b2d1ac7b1c1f38f0a0
AHReccese <amirhssin6rst@gmail.com>
2024-05-01 22:06:57 +0330

    add SOUND_FILE_NAME_EXIST_ERROR string

--- nava/params.py
+++ nava/params.py
@@ -7,7 +7,7 @@ OVERVIEW = """
 A Python library for playing sound everywhere natively and securely.
 
 """
-
+SOUND_FILE_NAME_EXIST_ERROR = "Please provide an audio file name to play."
 SOUND_FILE_PLAY_ERROR = "Sound can not play due to some issues."
 SOUND_FILE_EXIST_ERROR = "Given sound file doesn't exist."
 SOUND_FILE_PATH_TYPE_ERROR = "Sound file's path should be a string."
