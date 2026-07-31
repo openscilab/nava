7ba27027e0d8fd86027e2332aff4ea31cbe1f3c3
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-08-01 01:17:26 +0330

    fix : rename __play_proc_afplay to _play_proc_afplay

--- nava/functions.py
+++ nava/functions.py
@@ -260,7 +260,7 @@ def _play_afplay(sound_path: str, async_mode: bool = False, loop: bool = False) 
     if async_mode:
         sound_thread = NavaThread(loop,
                                   engine=Engine.AFPLAY,
-                                  target=__play_proc_afplay,
+                                  target=_play_proc_afplay,
                                   args=(sound_path,),
                                   daemon=True)
         sound_thread.start()
@@ -269,13 +269,13 @@ def _play_afplay(sound_path: str, async_mode: bool = False, loop: bool = False) 
         return sound_id
     else:
         while True:
-            proc = __play_proc_afplay(sound_path)
+            proc = _play_proc_afplay(sound_path)
             proc.wait()
             if not loop:
                 break
 
 
-def __play_proc_afplay(sound_path: str) -> subprocess.Popen:
+def _play_proc_afplay(sound_path: str) -> subprocess.Popen:
     """
     Create sound playing process using afplay.
 
